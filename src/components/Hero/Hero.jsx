c49f9abff9b83654d3cc12ab8c1a228cb9745455
Abdukadir001 <abdukodir374@gmail.com>
2024-10-28 17:14:34 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.scss'
+const Hero = () => {
+  return (
+    <>
+    <section className="section">
+      <div className="container">
+         <h1 className='hero__title'>A brand and product designer working with clients globally</h1>
+         <div className="hero__box">
+            <p>Expertise</p>
+            <p>Branding</p>
+            <p>Product</p>  
+            <p>Design Systems</p>
+         </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
